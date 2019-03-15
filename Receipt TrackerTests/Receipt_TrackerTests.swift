76af2c7123c0cda3e1f93adebefdfd8e202705ec
Scott Nicholes <scott93gps@gmail.com>
2019-03-14 22:15:20 -0600

    Initial Commit

---
+++ Receipt TrackerTests/Receipt_TrackerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Receipt_TrackerTests.swift
+//  Receipt TrackerTests
+//
+//  Created by Scott Nicholes on 3/14/19.
+//  Copyright © 2019 Scott Nicholes. All rights reserved.
+//
+
+import XCTest
+@testable import Receipt_Tracker
+
+class Receipt_TrackerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
