b722d251208c9ed087138ae9f5e34fd46f41fe83
Scott Nicholes <scott93gps@gmail.com>
2019-03-22 13:19:57 -0600

    Added Show Segue to AddReceipt View Controller and a ViewController file for AddReceipt ViewController

---
+++ Receipt Tracker/AddReceiptViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddReceiptViewController.swift
+//  Receipt Tracker
+//
+//  Created by Scott Nicholes on 3/22/19.
+//  Copyright © 2019 Scott Nicholes. All rights reserved.
+//
+
+import UIKit
+
+class AddReceiptViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
