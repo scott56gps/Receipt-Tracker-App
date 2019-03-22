26c32c36be7b0f5281013b80218991b0cd152186
Scott Nicholes <scott93gps@gmail.com>
2019-03-22 12:36:06 -0600

    Added id and initializer from Dictionary for Receipt Class.  Added Navigation Controller for navigation from Receipts ViewController.

--- Receipt Tracker/Receipt.swift
+++ Receipt Tracker/Receipt.swift
@@ -9,17 +9,49 @@
 import Foundation
 
 class Receipt {
+    var id: Int
     var vendorName: String
     var date: String
-    var total: Float
+    var total: String
     var items: [Item]?
     
-    init?(vendorName: String, date: String, total: Float, items: [Item]?) {
-        if (vendorName.isEmpty || date.isEmpty || total < 0) {
+    init?(id: Int, vendorName: String, date: String, total: String, items: [Item]?) {
+        if (vendorName.isEmpty || date.isEmpty || total.isEmpty) {
             return nil
         }
         
-        // Initialize Item Properties
+        // Initialize Object Properties
+        self.id = id
+        if (items != nil) {
+            self.items = items
+        }
+        self.vendorName = vendorName
+        self.date = date
+        self.total = total
+    }
+    
+    init?(receiptDictionary: [String : Any]) {
+        guard let id = receiptDictionary["id"]! as? Int else {
+            return nil
+        }
+        guard let vendorName = receiptDictionary["vendor_name"]! as? String else {
+            return nil
+        }
+        guard var date = receiptDictionary["date"]! as? String else {
+            return nil
+        }
+        guard var total = receiptDictionary["total"] as? String else {
+            return nil
+        }
+        let items = receiptDictionary["items"] as? [Item]
+        
+        // Format the values for display
+        let startIndex = date.index(date.startIndex, offsetBy: 10)
+        date.removeSubrange(startIndex...)
+        
+        total.insert("$", at: total.startIndex)
+        
+        self.id = id
         if (items != nil) {
             self.items = items
         }
