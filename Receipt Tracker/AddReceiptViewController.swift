6c12be0adf8b6dfbdece454c3339310395dbd576
Scott Nicholes <scott93gps@gmail.com>
2019-03-22 16:32:36 -0600

    Added table view outlet to add-receipt view controller.

--- Receipt Tracker/AddReceiptViewController.swift
+++ Receipt Tracker/AddReceiptViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class AddReceiptViewController: UIViewController {
-
+    // MARK: Outlets
+    @IBOutlet weak var receiptInputTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
